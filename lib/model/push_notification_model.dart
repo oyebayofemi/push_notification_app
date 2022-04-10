e10e6ab4873c2935c077dc0419c1861c09a7b0e8
oyebayofemi <61966651+oyebayofemi@users.noreply.github.com>
2022-04-10 15:15:22 +0100

    creating notification model

---
+++ lib/model/push_notification_model.dart
@@ -0,0 +1,8 @@
+class PushNotificationModel {
+  String? title;
+  String? body;
+  String? dataTitle;
+  String? dataBody;
+
+  PushNotificationModel({this.title, this.body, this.dataTitle, this.dataBody});
+}
