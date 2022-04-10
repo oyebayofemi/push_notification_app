c35081e7a25947f8660a4355de435a61e57b17ab
oyebayofemi <61966651+oyebayofemi@users.noreply.github.com>
2022-04-10 14:53:11 +0100

    adding dependencies

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Push Notification'),
+        ),
+        body: Column());
+  }
+}
