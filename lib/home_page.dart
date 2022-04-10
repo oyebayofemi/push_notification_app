467e0452bd37e1fbfb0563adb5cdef2bfe2467f3
oyebayofemi <61966651+oyebayofemi@users.noreply.github.com>
2022-04-10 15:45:41 +0100

    registering notification

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:push_notification_app/model/push_notification_model.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -6,6 +9,40 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late final FirebaseMessaging _messaging;
+  late int _totalNotificationCounter;
+
+  PushNotificationModel? _notificationInfo;
+
+  void registerNotification() async {
+    await Firebase.initializeApp();
+    _messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      sound: true,
+      badge: true,
+      provisional: false,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted the permission');
+
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        PushNotificationModel notification = PushNotificationModel(
+            body: message.notification!.body,
+            title: message.notification!.title,
+            dataBody: message.data['body'],
+            dataTitle: message.data['title']);
+
+        setState(() {
+          _totalNotificationCounter++;
+          _notificationInfo = notification;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
